544c2718b9cec0fa0a51177f416bf8ef16400860
Rakha A.S <rakha.amanta@gmail.com>
2026-09-24 18:22:39 +0700

    Configure and test Docker for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // listen on 0.0.0.0, not just localhost — required so the container's port is reachable from your host machine
+    port: 5173,
+     },
 });
